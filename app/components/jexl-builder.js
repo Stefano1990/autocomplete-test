91a256c751dc326e353b94ffa916e5802680fbd3
Stephan Hug <stefano.hug@gmail.com>
2021-05-11 07:27:38 +0200

    refactoring

--- app/components/jexl-builder.js
+++ app/components/jexl-builder.js
@@ -1,7 +1,21 @@
+/* eslint-disable prettier/prettier */
 import Component from '@glimmer/component';
 import jexl from 'jexl';
 import { action } from '@ember/object';
 import { tracked } from '@glimmer/tracking';
+
+// ! Hardcoded for MVP
+const QUESTIONS = [
+  '"test-question-1"',
+  '"something-question-2"',
+  '"else-question-3"',
+];
+const ANSWERS = {
+  'test-question-1': ['"yes"', '"no"'],
+  'something-question-2': ['"1"', '"2"', '"3"'],
+  'else-question-3': ['"maybe?"'],
+};
+const COMPARATORS = ['==', '!=', '&&', '||'];
 
 const transforms = [
   { answer: (questions) => questions.map((question) => question) },
@@ -12,6 +26,102 @@ Object.entries(transforms).map(([key, transform]) => {
 });
 
 export default class JexlBuilderComponent extends Component {
+  ast
+  astIsInvalid
+  @tracked jexlExpression = ''
+  ast
+
+  get slug () {
+    const matches = this.jexlExpression.match(/(\w+)$/g);
+    return matches ? matches[0] : null
+  }
+
+  get filteredQuestions () {
+    if (!this.slug) {
+      return QUESTIONS;
+    }
+    return QUESTIONS.filter((q) => q.includes(this.slug));
+  }
+
+  rightMostAstType (ast) {
+    if (!ast.right) {
+      return ast
+    }
+    this.rightMostAstType(ast.right)
+  }
+
+  get suggestions () {
+    if (!this.ast) { return this.filteredQuestions }
+    switch(this.ast.type) {
+      case 'BinaryExpression':
+        const rightMostAstType = this.rightMostAstType(this.ast)
+        console.log("🦠 this.rightMostAstType(this.ast):", rightMostAstType)
+        debugger
+        if (this.ast.right.type === 'Literal') {
+          return this.lastOperationIsAComparator ? this.possibleAnswers : COMPARATORS
+        }
+        if (this.ast.right.type === 'Identifier') {
+          return this.filteredQuestions;
+        }
+        if (this.ast.right.type === 'FunctionCall') {
+          return this.lastOperationIsAComparator ? this.possibleAnswers : COMPARATORS
+        }
+        break;
+      case 'FunctionCall':
+        return this.lastOperationIsAComparator ? this.possibleAnswers : COMPARATORS
+      case 'Identifier':
+        return COMPARATORS;
+      default:
+        return this.filteredQuestions
+    }
+    return this.filteredQuestions
+  }
+
+  get lastOperationIsAComparator() {
+    return COMPARATORS.includes(this.jexlExpression.slice(-3, -1))
+  }
+
+  get possibleAnswers () {
+    if (this.ast.type === 'FunctionCall') {
+      const lastArg = this.ast.args[this.ast.args.length - 1];
+      if (lastArg.type === 'Literal') {
+        return ANSWERS[lastArg.value];
+      }
+    }
+
+    if (this.ast.type === 'BinaryExpression') {
+      if (this.ast.right.args) {
+        return ANSWERS[this.ast.right.args[0].value];
+      }
+    }
+
+    return this.filteredQuestions
+  }
+
+  @action
+  recomputeAst() {
+    try {
+      const result = jexl.createExpression(this.jexlExpression)._getAst();
+      this.astIsValid = true;
+      this.ast = result;
+    } catch (e) {
+      this.astIsValid = false;
+    }
+    console.log("🦠 this.ast:", this.ast)
+  }
+
+  @action
+  selectSuggestion(suggestion) {
+    const jexlToAppend = this.appendAnswerTransformIfRequired(suggestion);
+    this.jexlExpression = this.jexlExpression += jexlToAppend
+    this.recomputeAst()
+  }
+
+  appendAnswerTransformIfRequired(jexlToAppend) {
+    return QUESTIONS.includes(jexlToAppend) ? jexlToAppend += '|answer ' : jexlToAppend += ' '
+  }
+
+  /*
   @tracked jexlExpression = ''; // ! not sure how to do this
   @tracked astIsInvalid = false;
   lastSuccessfulAst;
@@ -73,4 +183,5 @@ export default class JexlBuilderComponent extends Component {
   pickFirstSuggestion() {
     console.log("🦠 'in here':", 'in here');
   }
+  */
 }
--- app/components/suggestions.js
+++ app/components/suggestions.js
@@ -1,101 +1,96 @@
+/* eslint-disable prettier/prettier */
 import Component from '@glimmer/component';
 import { action } from '@ember/object';
 
-// ! Hardcoded for MVP
-const QUESTIONS = [
-  '"test-question-1"',
-  '"something-question-2"',
-  '"else-question-3"',
-];
-const ANSWERS = {
-  'test-question-1': ['"yes"', '"no"'],
-  'something-question-2': ['"1"', '"2"', '"3"'],
-  'else-question-3': ['"maybe?"'],
-};
-const COMPARATORS = ['==', '!=', '&&', '||'];
+
 
 export default class SuggestionsComponent extends Component {
-  slug;
+  
+  // slug;
 
-  get filteredQuestions() {
-    const matches = this.args.jexlExpression.match(/[\w-]+$/g);
-    if (matches) {
-      this.slug = matches[0]; // ! not sure how to do this in ember
-    }
+  // get filteredQuestions() {
+  //   const matches = this.args.jexlExpression.match(/[\w-]+$/g);
+  //   if (matches) {
+  //     this.slug = matches[0]; // ! not sure how to do this in ember
+  //   }
 
-    if (!this.slug) {
-      return QUESTIONS;
-    }
-    return QUESTIONS.filter((question) => question.includes(this.slug));
-  }
+  //   if (!this.slug) {
+  //     return QUESTIONS;
+  //   }
+  //   return QUESTIONS.filter((question) => question.includes(this.slug));
+  // }
 
-  get suggestions() {
-    if (!this.args.ast) {
-      return this.filteredQuestions;
-    }
+  // get suggestions() {
+  //   if (!this.args.ast) {
+  //     return this.filteredQuestions;
+  //   }
 
-    const lastOperationIsAComparator = COMPARATORS.includes(
-      this.args.jexlExpression.slice(-2)
-    );
-    if (lastOperationIsAComparator) {
-      return this.fetchPossibleAnswers();
-    }
+  //   const lastOperationIsAComparator = COMPARATORS.includes(
+  //     this.args.jexlExpression.slice(-2)
+  //   );
+  //   if (lastOperationIsAComparator) {
+  //     return this.fetchPossibleAnswers();
+  //   }
 
-    if (this.args.ast.type === 'BinaryExpression') {
-      if (this.args.ast.right.type === 'Literal') {
-        return COMPARATORS;
-      }
-      if (this.args.ast.right.type === 'Identifier') {
-        return this.filteredQuestions;
-      }
-      return COMPARATORS;
-    }
-    if (this.args.ast.type === 'FunctionCall') {
-      return COMPARATORS;
-    }
+  //   if (this.args.ast.type === 'BinaryExpression') {
+  //     if (this.args.ast.right.type === 'Literal') {
+  //       return COMPARATORS;
+  //     }
+  //     if (this.args.ast.right.type === 'Identifier') {
+  //       return this.filteredQuestions;
+  //     }
+  //     return COMPARATORS;
+  //   }
+  //   if (this.args.ast.type === 'FunctionCall') {
+  //     return COMPARATORS;
+  //   }
 
-    if (this.args.ast.type === 'Identifier') {
-      return this.filteredQuestions;
-    }
+  //   if (this.args.ast.type === 'Identifier') {
+  //     return this.filteredQuestions;
+  //   }
 
-    return QUESTIONS;
-  }
+  //   return QUESTIONS;
+  // }
 
-  fetchQuestions = function () {
-    return QUESTIONS;
-  };
+  // fetchQuestions = function () {
+  //   return QUESTIONS;
+  // };
 
-  fetchPossibleAnswers = function () {
-    const ast = this.args.ast;
+  // fetchPossibleAnswers = function () {
+  //   const ast = this.args.ast;
 
-    if (ast.type === 'FunctionCall') {
-      const lastArg = ast.args[ast.args.length - 1];
-      if (lastArg.type === 'Literal') {
-        return ANSWERS[lastArg.value];
-      }
-    }
+  //   if (ast.type === 'FunctionCall') {
+  //     const lastArg = ast.args[ast.args.length - 1];
+  //     if (lastArg.type === 'Literal') {
+  //       return ANSWERS[lastArg.value];
+  //     }
+  //   }
 
-    if (ast.type === 'BinaryExpression') {
-      if (ast.right.args) {
-        return ANSWERS[ast.right.args[0].value];
-      }
-    }
+  //   if (ast.type === 'BinaryExpression') {
+  //     if (ast.right.args) {
+  //       return ANSWERS[ast.right.args[0].value];
+  //     }
+  //   }
 
-    return this.fetchQuestions();
-  };
+  //   return this.fetchQuestions();
+  // };
 
-  @action
-  select(suggestion) {
-    const jexlToAppend = this.appendAnswerTransformIfRequired(suggestion);
+  // @action
+  // select(suggestion) {
+  //   const jexlToAppend = this.appendAnswerTransformIfRequired(suggestion);
 
-    this.args.onSelection(jexlToAppend, this.slug);
-    this.slug = null;
-  }
+  //   this.args.onSelection(jexlToAppend, this.slug);
+  //   this.slug = null;
+  // }
 
-  appendAnswerTransformIfRequired(jexlToAppend) {
-    if (QUESTIONS.includes(jexlToAppend)) {
-      return (jexlToAppend += '|answer');
-    }
-    return jexlToAppend;
-  }
+  // appendAnswerTransformIfRequired(jexlToAppend) {
+  //   if (QUESTIONS.includes(jexlToAppend)) {
+  //     return (jexlToAppend += '|answer');
+  //   }
+  //   return jexlToAppend;
+  // }
+
+  // onPickFirstSuggestion () {
+    
+  // }
 }
